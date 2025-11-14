264258e7901971536e564cc35bea5120e21f24ae
nerinacengic1 <ncengic1@etf.unsa.ba>
2025-11-14 16:52:17 +0100

    lv4

---
+++ Z2/src/Main.java
@@ -0,0 +1,16 @@
+import Enums.VrstaMesa;
+import Hrana.Meso;
+import Hrana.Povrce;
+import Hrana.Voce;
+import Interfaces.IZdravlja;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    }
+}
